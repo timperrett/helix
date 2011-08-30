059edf95f2e3f6d1d90ec8a05b6b1c5b21b5516d
Timothy Perrett <timothy@getintheloop.eu>
2011-08-30 23:37:12 +0100

    Added cool model for adding project versions

--- src/main/webapp/js/helix.js
+++ src/main/webapp/js/helix.js
@@ -16,71 +16,71 @@ String.prototype.format = function(i, safe, arg) {
   return format;
 }();
 
-$(function(){
-  
-  function toggleLoadingSpinner(){
-    var icon = $('#loading')
-    if(icon.is(':visible')){
-      icon.hide();
-    } else {
-      icon.show();
-    }
-  }
-  
-  function isGithubUserRepoFormat(input){
-    return /^([a-z0-9-]+)\/([a-z0-9-]+)$/.test(input)
-  }
-  
-  function displaySourceUrlError(msg){
-    var div = $('#sourceurl-wrapper');
-    $('#sourceurl-error').text(msg)
-    if(!div.hasClass('error')){
-      div.addClass('error')
-    }
-  }
-  
-  function clearSourceUrlErrors(){
-    var div = $('#sourceurl-wrapper');
-    if(div.hasClass('error')){
-      div.removeClass('error');
-      $('#sourceurl-error').text('');
-    }
-  }
-  
-  $error = true;
-  
-  $('#sourceurl').typing({
-    start: function(event, $elem){
-      clearSourceUrlErrors();
-    },
-    stop: function(event, $elem){
-      var input = $elem.val();
-      if(isGithubUserRepoFormat(input)){
-        toggleLoadingSpinner();
-        var parts = input.split('/');
-        gh.repo(parts[0], parts[1]).show(function(data){
-          $error = false;
-          $('#description').val(data.repository.description)
-          $('#projectname').val(data.repository.name)
-          $('#mainform').slideDown();
-          $('button.primary').prop('disabled', false);
-          clearSourceUrlErrors();
-        });
-        // nasty hack to work out if the github call
-        // worked or not
-        console.log($('#mainform').css('display'));
-        if($error){
-          displaySourceUrlError('Unable to find that repository on GitHub')
-          $('#mainform').slideUp();
-        }
-        toggleLoadingSpinner();
-      } else {
-        // display error msg
-        displaySourceUrlError('That is not a valid GitHub user/repo format')
-      }
-    },
-    delay: 500
-  });
-  
-});
+// $(function(){
+//   
+//   function toggleLoadingSpinner(){
+//     var icon = $('#loading')
+//     if(icon.is(':visible')){
+//       icon.hide();
+//     } else {
+//       icon.show();
+//     }
+//   }
+//   
+//   function isGithubUserRepoFormat(input){
+//     return /^([a-z0-9-]+)\/([a-z0-9-]+)$/.test(input)
+//   }
+//   
+//   function displaySourceUrlError(msg){
+//     var div = $('#sourceurl-wrapper');
+//     $('#sourceurl-error').text(msg)
+//     if(!div.hasClass('error')){
+//       div.addClass('error')
+//     }
+//   }
+//   
+//   function clearSourceUrlErrors(){
+//     var div = $('#sourceurl-wrapper');
+//     if(div.hasClass('error')){
+//       div.removeClass('error');
+//       $('#sourceurl-error').text('');
+//     }
+//   }
+//   
+//   $error = true;
+//   
+//   $('#sourceurl').typing({
+//     start: function(event, $elem){
+//       clearSourceUrlErrors();
+//     },
+//     stop: function(event, $elem){
+//       var input = $elem.val();
+//       if(isGithubUserRepoFormat(input)){
+//         toggleLoadingSpinner();
+//         var parts = input.split('/');
+//         gh.repo(parts[0], parts[1]).show(function(data){
+//           $error = false;
+//           $('#description').val(data.repository.description)
+//           $('#projectname').val(data.repository.name)
+//           $('#mainform').slideDown();
+//           $('button.primary').prop('disabled', false);
+//           clearSourceUrlErrors();
+//         });
+//         // nasty hack to work out if the github call
+//         // worked or not
+//         console.log($('#mainform').css('display'));
+//         if($error){
+//           displaySourceUrlError('Unable to find that repository on GitHub')
+//           $('#mainform').slideUp();
+//         }
+//         toggleLoadingSpinner();
+//       } else {
+//         // display error msg
+//         displaySourceUrlError('That is not a valid GitHub user/repo format')
+//       }
+//     },
+//     delay: 500
+//   });
+//   
+// });
 
