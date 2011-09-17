fcf1e241072a56a0111b2764e190088e3076ffa0
Timothy Perrett <timothy@getintheloop.eu>
2011-09-18 00:18:16 +0100

    corrected some stuff in the TODO

--- src/main/scala/helix/github/scoring.scala
+++ src/main/scala/helix/github/scoring.scala
@@ -9,6 +9,13 @@ trait GithubScoring extends Scoring {
     private case class Commits(count: Int)
     
     /*
+    https://api.github.com/repos/djspiewak/42omega
+    created_at
+
+    https://api.github.com/repos/djspiewak/42omega/commits/5ddbd17e3028ba0c2d9cd015343fbcac505e49b7
+    size of array
+    tail sha
+    
     
     + watchers 
     + commiters
