83e452f8745da2286b2ea6dc720145e7f7c56cf9
Timothy Perrett <timothy@getintheloop.eu>
2011-09-27 20:31:56 +0200

    More updates

--- core/src/main/scala/helix/domain/structures.scala
+++ core/src/main/scala/helix/domain/structures.scala
@@ -4,7 +4,63 @@ import com.novus.salat._
 import com.novus.salat.global._
 import com.novus.salat.annotations.{Key,Salat}
 import com.mongodb.casbah.Imports._
-// import net.liftweb.util.Helpers
+import org.joda.time.DateTime
+
+case class Project(
+  @Key("_id") id: ObjectId = new ObjectId, 
+  name: String, 
+  headline: Option[String] = None,
+  description: Option[String] = None,
+  groupId: Option[String] = None,
+  artifactId: Option[String] = None,
+  versions: Map[String, String] = Map.empty,
+  modules: Map[String, String] = Map.empty,
+  usagePhase: Option[String] = None, // compile, test, jetty etc
+  repositoryURL: Option[String] = None, // maven repo
+  sourceURL: Option[String] = None, // github repo
+  addedBy: Option[String] = None, // addedBy represents the adder's github login
+  addedAt: java.util.Date = new DateTime().toDate,
+  createdAt: java.util.Date = new DateTime().toDate, // when the github repo was made
+  updatedAt: Long = new DateTime().getMillis, // updated every time the async task runs
+  contributors: List[Contributor] = Nil,
+  tags: List[Tag] = Nil,
+  // asyncrnous
+  contributorCount: Long = 1L,
+  forkCount: Long = 1L,
+  watcherCount: Long = 1L,
+  activityScore: Double = 0D,
+  setupComplete: Boolean = false
+){
+  import helix.util.Random.randomSelect
+  import helix.util.Hex
+  
+  def randomContributor: Option[Contributor] = 
+    if(contributors.isEmpty) None
+    else randomSelect(1, contributors).headOption
+  
+  def versionsDecoded = 
+    versions.map(x => new String(Hex.decode(x._1)) -> x._2)
+  
+  def activity: Activity = 
+    List(Obsolete, Quiet, Moderate, Fair, Busy, Hectic
+      ).find(_.judge(activityScore)).getOrElse(UnknownActivity)
+  
+  val usernameAndRepository = sourceURL.map(_.substring(19))
+}
+
+case class Version(number: String, description: String, scalaVersions: List[ScalaVersion])
+
+case class Contributor(
+  login: String, 
+  name: Option[String] = None, 
+  avatar: Option[String] = None,
+  contributions: Int = 0,
+  style: String = "User"
+){
+  def picture = avatar getOrElse "http://gravatar.com/unknown"
+}
+
+case class Tag(name: String)
 
 object ScoringPoints {
   val Zero = 0D
@@ -39,62 +95,6 @@ object Busy extends Activity(s => s > Band4 && s <= Band5){
 object Hectic extends Activity(s => s > Band5){
   override def toString = "Hectic"
 }
-
-import org.joda.time.DateTime
-
-case class Project(
-  @Key("_id") id: ObjectId = new ObjectId, 
-  name: String, 
-  headline: Option[String] = None,
-  description: Option[String] = None,
-  groupId: Option[String] = None,
-  artifactId: Option[String] = None,
-  versions: Map[String, String] = Map.empty,
-  usagePhase: Option[String] = None, // compile, test, jetty etc
-  repositoryURL: Option[String] = None, // maven repo
-  sourceURL: Option[String] = None, // github repo
-  addedBy: Option[String] = None, // addedBy represents the adder's github login
-  addedAt: java.util.Date = new DateTime().toDate,
-  createdAt: java.util.Date = new DateTime().toDate, // when the github repo was made
-  updatedAt: Long = new DateTime().getMillis, // updated every time the async task runs
-  contributors: List[Contributor] = Nil,
-  tags: List[Tag] = Nil,
-  // asyncrnous
-  contributorCount: Long = 1L,
-  forkCount: Long = 1L,
-  watcherCount: Long = 1L,
-  activityScore: Double = 0D,
-  latestSHA: Option[String] = None,
-  setupComplete: Boolean = false
-){
-  import helix.util.Random.randomSelect
-  import helix.util.Hex
-  
-  def randomContributor: Option[Contributor] = 
-    if(contributors.isEmpty) None
-    else randomSelect(1, contributors).headOption
-  
-  def versionsDecoded = 
-    versions.map(x => new String(Hex.decode(x._1)) -> x._2)
-  
-  def activity: Activity = 
-    List(Obsolete, Quiet, Moderate, Fair, Busy, Hectic
-      ).find(_.judge(activityScore)).getOrElse(UnknownActivity)
-  
-  val usernameAndRepository = sourceURL.map(_.substring(19))
-}
-
-case class Contributor(
-  login: String, 
-  name: Option[String] = None, 
-  avatar: Option[String] = None,
-  contributions: Int = 0,
-  style: String = "User"
-){
-  def picture = avatar getOrElse "http://gravatar.com/unknown"
-}
-
-case class Tag(name: String)
 
 @Salat
 trait ReleaseType
